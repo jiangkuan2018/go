3df27df6ce8c2723830bdcb52ca9e7e020b29493
jiangkuan <jiangkuan@shuidi-inc.com>
2022-03-21 21:21:33 +0800

    update

--- gopl/ch1/dup2.go
+++ gopl/ch1/dup2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
@@ -11,13 +12,28 @@ func main() {
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
 	} else {
-
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Println(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
 	}
+	fmt.Println(counts)
 }
 
 func countLines(f *os.File, count map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		count[input.Text()]++
+		str := input.Text()
+		count[str]++
+	}
+	for line, n := range count {
+		if n > 1 {
+			fmt.Println(line)
+		}
 	}
 }
